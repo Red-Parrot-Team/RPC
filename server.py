8505e5f85d262d830854bba3fb3e30fbaeff86ef
Vadim <vadimpir2003@mail.ru>
2021-03-15 20:04:07 +0700

    Переписал консольный чат на twisted

--- server.py
+++ server.py
@@ -1,41 +1,40 @@
-import asyncio
+from twisted.internet import reactor, protocol, endpoints
+from functions import *
 
-clients = []
-
-class ChatProtocol(asyncio.Protocol):
+class ServerProto(protocol.Protocol):
     def __init__(self):
-        self.connections = []
-
-    def connection_made(self, transport):
-        self.transport = transport
-        self.peername = transport.get_extra_info('peername')
-        print("Подключился:", self.peername)
-        clients.append(self)
-
-    def connection_lost(self, exc):
-        print("Отключился:", self.peername)
-        clients.remove(self)
-
-    def data_received(self, data):
-        print(self.peername, "отправил:", data.decode('utf-8'))
-        for client in clients:
-            if client is not self:
-                client.transport.write(data )
-
-if __name__ == "__main__":
-    print("Запуск...")
-    loop = asyncio.get_event_loop()
-    coro = loop.create_server(ChatProtocol, host='localhost', port=9876)
-    server = loop.run_until_complete(coro)
-
-    print("Сервер запущен")
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
         pass
-    finally:
-        server.close()
-        asyncio.run(server.wait_closed())
-    loop.close()
 
+    def connectionMade(self):
+        peer = self.transport.getPeer()
+        self.peerAddress = formatAddress(peer)
+        
+        joinMessage = "[{}] joined the chat".format(self.peerAddress) 
+        print(joinMessage)
+        self.sendAllWithoutThis(joinMessage.encode('utf-8'))
+        self.factory.clients.add(self)
+
+    def connectionLost(self, reason):
+        message = "[{}] left the chat".format(self.peerAddress) 
+        print(message)
+        self.sendAllWithoutThis(message.encode('utf-8'))
+        self.factory.clients.remove(self)
+
+    def dataReceived(self, data):
+        message = "[{}]: {}".format(self.peerAddress, data.decode('utf-8'))
+        print(message)
+        self.sendAllWithoutThis(message.encode('utf-8'))
+
+    def sendAllWithoutThis(self, msg):
+        for client in self.factory.clients:
+            if not(client is self):
+                client.transport.write(msg)
+
+class PubFactory(protocol.ServerFactory):
+    protocol = ServerProto
+    def __init__(self):
+        self.clients = set()
+
+
+reactor.listenTCP(8000, PubFactory())
+reactor.run()
